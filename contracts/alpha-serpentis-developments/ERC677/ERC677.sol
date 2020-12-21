a5d12af0118ed6e838cd80d9c0cd43b4dd673727
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2020-12-21 14:28:39 -0600

    ERC677 implementation

--- contracts/alpha-serpentis-developments/ERC677/ERC677.sol
+++ contracts/alpha-serpentis-developments/ERC677/ERC677.sol
@@ -3,9 +3,17 @@
 pragma solidity ^0.7.0;
 
 import "../../openzeppelin/token/ERC20/ERC20.sol";
+import "./ERC677Receiver.sol";
 
-abstract contract ERC677 is ERC20 {
+contract ERC677 is ERC20 {
     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
 
-    function transferAndCall(address _receiver, uint256 _amount, bytes memory _data) public virtual returns(bool success);
+    function transferAndCall(address _receiver, uint256 _amount, bytes memory _data) public returns(bool success) {
+        transfer(_receiver, _amount); // Call ERC20 transfer() method, returns a bool
+        ERC677Receiver receiverContract;
+
+        receiverContract = ERC677Receiver(_receiver);
+        receiverContract.onTokenTransfer(_msgSender(), _amount, _data); // Call receiver contract; does not guarantee a return if receiver does not have onTokenTransfer()
+        return true;
+    }
 }
--- contracts/alpha-serpentis-developments/ERC677/ERC677Receiver.sol
+++ contracts/alpha-serpentis-developments/ERC677/ERC677Receiver.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
+/**
+ * @dev Implementation for a receiving contract to use. 
+ */
 abstract contract ERC677Receiver {
-    event Transfer(address, address, uint256, bytes);
-
     function onTokenTransfer(address _from, uint256 _amount, bytes memory _data) public virtual returns(bool success);
 }
